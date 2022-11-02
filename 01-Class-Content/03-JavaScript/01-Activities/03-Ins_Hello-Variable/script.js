76eddc683c646b6460c43e647b98cc9ac8c85944
Ryan Vern Sheffer <ryan.sheffer@familysearch.org>
2022-11-02 17:44:49 -0600

    Add Week 3 content locally

---
+++ 01-Class-Content/03-JavaScript/01-Activities/03-Ins_Hello-Variable/script.js
@@ -0,0 +1,32 @@
+// Declares student variable using var keyword 
+var studentName;
+
+// console.log(studentName);
+
+// Uses assignment operator(=) to assign a value
+studentName = "Abdul";
+var studentAge = 32;
+
+// console.log(typeof studentAge);
+
+
+// // To re-assign a variable, use only the variable's name  
+studentName = "Tonya";
+studentAge = 52;
+
+// 1234567
+// 1.5 etc.
+
+
+
+console.log(studentName);
+console.log(studentAge);
+// // To access a value stored in a variable, use the variable's name
+// console.log(studentName);
+
+// //To combine the message with a variable value use the concatenation operator(+)
+// //Logs "My name is "
+console.log("My name is ");
+
+// // Logs "My name is Tonya"
+console.log("My name is " + studentName);
