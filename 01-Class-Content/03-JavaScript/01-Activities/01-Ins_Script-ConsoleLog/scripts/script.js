52ae86f02f739fdab001403156db712c16b3073e
matt b <75261103+Mbowman1221@users.noreply.github.com>
2022-11-13 17:51:59 -0800

    Delete 01-Class-Content directory

--- 01-Class-Content/03-JavaScript/01-Activities/01-Ins_Script-ConsoleLog/scripts/script.js
+++
@@ -1,9 +0,0 @@
-// External files make your code organized and easier to maintain
-console.log("This is the log for the 🔥EXTERNAL🔥JavaScript");
-
-// The Invokation of .log or the usage
-console.log("Hello World!!");
-
-
-console.dir(console);
-console.table(console);
--- 01-Class-Content/03-JavaScript/03-Algorithms/03-log-nums/Unsolved/log-nums.js
+++
@@ -1,9 +0,0 @@
-// Write code to print all numbers from 1 to `num`
-// Assume `num` will be a positive number
-
-let logNums = function(num) {
-    for (let index = 0; index < num; index++) {
-        var nextNum = index + 1;
-        console.log(nextNum);   
-    }
-};
